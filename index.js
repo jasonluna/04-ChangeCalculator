4cef45dbfad2d8589c80ee6d1891282c957c4b5d
jasonluna <jasonluna92054@gmail.com>
2016-06-17 11:22:57 -0700

    peer review change calculator

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 $(function() {
 	$('#calculate').on('click', function() {
+		// gets all the user inputs
 		var totalDue = $('#totalDue').val();
 		var totalPaid = $('#totalPaid').val();
+		// gets the change from the transaction and rounds it down to only get dollars
 		var change = totalPaid - totalDue ;
 		var dollars = Math.floor(change);
-		change = change - dollars;
+// takes the remaining dollars away and only leaves the coins
+        change = change - dollars;
+        // makes it easier to work with the change we have left over 
 		 change = change * 100;
+// gives the remainder for the quarters
 		var quarter = Math.floor(change/25);
 		change = change % 25;
+		// gives remainder for dimes
 		var dime = Math.floor(change/10);
 		change = change % 10;
+		// gives remainder for nickles
 		var nickle = Math.floor(change/5);
 		change = change % 5;
+		// gives remainder for pennies
 		var penny = Math.round(change);
 		
 
 
-
+// printing the change on the html page
 		$('#dollars').html(dollars);
 		$('#quarter').html(quarter);
 		$('#dime').html(dime);
